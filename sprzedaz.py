d2eb149d15a253eb55c87e2d9b0fe00eb3cfaad7
Łukasz Ł <ukaszek1001@gmail.com>
2021-11-01 17:07:39 +0100

    dodoano plik ze sprzedaza

---
+++ sprzedaz.py
@@ -0,0 +1,18 @@
+import sys
+from files import FileHandler
+from main import Warehouse
+
+file_path_output = "out.txt"
+file_path_input = sys.argv[1]
+komenda = sys.argv[1:]
+
+historia = FileHandler(file_path_read=file_path_input, file_path_write=file_path_output)
+historia.read_history()
+
+warehouse = Warehouse(historia.history)
+warehouse.oblicz_aktualny_stan()
+warehouse.sprzedaz(komenda)
+
+komenda = ["sprzedaz"] + komenda[1:]
+historia.history.append(komenda)
+historia.write_history(historia.history)
