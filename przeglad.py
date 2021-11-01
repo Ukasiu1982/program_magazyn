65d60c56c1e48c504a7800831cc84fcd738922cc
Łukasz Ł <ukaszek1001@gmail.com>
2021-11-01 17:06:21 +0100

    dodano plik wyswietlajacy przeglad

---
+++ przeglad.py
@@ -0,0 +1,14 @@
+import sys
+from files import FileHandler
+from main import Warehouse
+
+file_path_output = "out.txt"
+file_path_input = sys.argv[1]
+komenda = sys.argv[1:]
+
+historia = FileHandler(file_path_read=file_path_input, file_path_write=file_path_output)
+historia.read_history()
+
+warehouse = Warehouse(historia.history)
+warehouse.oblicz_aktualny_stan()
+warehouse.przeglad(komenda)
