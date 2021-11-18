66fd1e011e0c03b83d9b00ef6e1fd55c3120032b
Łukasz Ł <ukaszek1001@gmail.com>
2021-11-18 18:34:18 +0100

    naprawiono plik magazyn

--- magazyn.py
+++ magazyn.py
@@ -4,11 +4,12 @@ from main import Magazyn
 
 file_path_output = "out.txt"
 file_path_input = sys.argv[1]
-
+file_path_products = sys.argv[2:]
 
 historia = FileHandler(file_path_read=file_path_input, file_path_write=file_path_output)
 historia.read_history()
 
+
 magazyn = Magazyn(historia.history)
 magazyn.oblicz_aktualny_stan()
-magazyn.stan_magazynu()
+magazyn.stan_magazynu(file_path_products)
